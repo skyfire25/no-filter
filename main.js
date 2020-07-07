f0ddb4729db3c4239b34664feaeafdbbcc5ca3cd
Carlos L Molina <carlos.molina@codeimmersives.com>
2020-07-07 14:01:49 -0400

    changes

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const onlyOdds = function(num){
+const nums = [];
+for (const num of nums){
+if(num === 1 || num === 3){
+nums.push(num)
+}
+}
+return nums
+}
 
+const onlyEvens = function(even){
+  const numz = [];
+  for(const even of evens){
+    if(even === 2 || even === 4){
+      numz.push(even)
+    }
+  }
+  return numz
+}
 
 
 
